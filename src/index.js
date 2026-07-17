199951740e71e80c688c50d48960177fe5e525b0
vaneck237 <vaneckbilongo@gmail.com>
2026-07-17 11:55:08 +0100

    remove french translation

--- src/index.js
+++ src/index.js
@@ -8,13 +8,12 @@ import { EmailSettingsPage } from "./pages/EmailSettingsPage";
 import ReportsPage from "./pages/ReportsPage";
 
 import messages_en from "./translations/en.json";
-import messages_fr from "./translations/fr.json";
 
 import ReportDefinitionEditorDialog from "./components/ReportDefinitionEditorDialog";
 import ReportPicker from "./components/ReportPicker";
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: "en", messages: messages_en }, { key: "fr", messages: messages_fr }],
+  "translations": [{ key: "en", messages: messages_en }],
   "refs": [
     { key: "tools.reports", ref: "tools/reports" },
     { key: "tools.ReportDefinitionEditorDialog", ref: ReportDefinitionEditorDialog },
